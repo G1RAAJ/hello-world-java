51ddcb79c4413f1571e7887ec92713899b7e42c2
G1RAAJ <jeevanraju.ch@gmail.com>
2025-01-21 21:48:31 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
-public class HelloWorld {
+public class Hello Universe {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    System.out.println("Hello Universe!");
   }
 }
